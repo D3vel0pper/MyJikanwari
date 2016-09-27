bd5e698327a33fba88f7d4198ee069858be6440b
D3vel0pper <toamaindex@gmail.com>
2016-09-27 22:22:10 +0900

    fix #13

--- app/src/main/java/d3vel0pper/com/myjikanwari/fragment/MainFragment.java
+++ app/src/main/java/d3vel0pper/com/myjikanwari/fragment/MainFragment.java
@@ -10,6 +10,9 @@ import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import d3vel0pper.com.myjikanwari.R;
 import d3vel0pper.com.myjikanwari.activity.MainActivity;
 import d3vel0pper.com.myjikanwari.adapter.ItemAdapter;
@@ -41,13 +44,20 @@ public class MainFragment extends Fragment {
 
         TextView testText = (TextView)view.findViewById(R.id.testText);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String testString = preferences.getString("dayofweek","");
+        Set<String> testString = preferences.getStringSet("dayofweek",null);
 //        PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("dayofweek","");
 //        PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("notifier_time","default");
 //        PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("number_periods","default");
-        String data = "dayofweek = {" + PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("dayofweek","default")
-                + "}\nnotifier_time = {" + PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("notifier_time","default")
-                + "}\nnumber_periods = {" + PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("number_periods","default");
+        String data = "dayofweek = {" ;
+        if(testString.size() != 0) {
+            for (int i = 0; i < testString.size(); i++) {
+                data = data + testString.toString();
+            }
+        } else {
+            data = data + "no-data";
+        }
+        data = data + "}\nnotifier_time = {" + PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("notifier_time","default")
+                + "}\nnumber_periods = {" + PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("number_periods","default") + "}";
         testText.setText(data);
 
         GridView gridView = (GridView)view.findViewById(R.id.item_grid);
